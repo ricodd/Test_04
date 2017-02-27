366db1022c4cd4030e012c0c19e2a2a2b93c7874
Sequarius <sequarius@users.noreply.github.com>
2017-02-27 20:47:57 +0800

    Update Test04Application.java

--- src/main/java/rico/demo/Test04Application.java
+++ src/main/java/rico/demo/Test04Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test04Application {
-
+//
     public static void main(String[] args) {
         SpringApplication.run(Test04Application.class, args);
 
